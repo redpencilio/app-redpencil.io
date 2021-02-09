938478e5c431198468b93954f8ed2cba12bf60de
Unknown <achrafatauil@hotmail.com>
2021-02-09 17:12:26 +0100

    strip elements from dispatcher

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -16,24 +16,16 @@ defmodule Dispatcher do
   ###############################################################
   # master-email-domain.lisp
   ###############################################################
-  match "/mailboxes/*path" do
-    Proxy.forward conn, path, "http://resource/mailboxes/"
+  post "/email-delivery/*path" do
+    Proxy.forward conn, path, "http://deliver-email-service/email-delivery/"
   end
 
-  match "/mail-folders/*path" do
+  get "/mail-folders/*path" do
     Proxy.forward conn, path, "http://resource/mail-folders/"
   end
 
-  match "/emails/*path" do
+  post "/emails/*path" do
     Proxy.forward conn, path, "http://resource/emails/"
-  end
-
-  match "/email-headers/*path" do
-    Proxy.forward conn, path, "http://resource/email-headers/"
-  end
-
-  post "/email-delivery/*path" do
-    Proxy.forward conn, path, "http://deliver-email-service/email-delivery/"
   end
 
   match _ do
